f85970287b0c96619d0a9242ada3374a98434e52
miamano <frinyo@hotmail.com>
2016-03-28 22:49:50 +0200

    Added ex 8

---
+++ Exercise8/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Logger
+    {
+        public List<string> LogPosts { get; private set; }
+
+        public Logger()
+        {
+            LogPosts = new List<string>();
+        }
+
+        public void Log(string message)
+        {
+            LogPosts.Add(message);
+        }
+    }
+}
---
+++ Exercise8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger myLogger = new Logger();
+            myLogger.Log("First message");
+            myLogger.Log("Second message");
+            myLogger.Log("Third message");
+
+            foreach(string logger in myLogger.LogPosts)
+            {
+                Console.WriteLine(logger);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
